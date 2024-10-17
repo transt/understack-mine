cc8f6f97d28abe972f6540c33df4e0b818fd8f12
Scott Tran <transt@gmail.com>
2024-10-17 15:56:07 -0500

    use matrix

--- ironic-images/keekz-ubuntu-extras/keekz-ubuntu-extras.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# ubuntu version
-export DIB_RELEASE=noble
-
-# devuser element - https://github.com/openstack/diskimage-builder/tree/master/diskimage_builder/elements/devuser
-
-diskimage-builder keekz-ubuntu-extras.yaml
